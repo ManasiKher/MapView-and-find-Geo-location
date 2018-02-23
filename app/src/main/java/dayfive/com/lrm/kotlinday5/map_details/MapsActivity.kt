ac33f5e65e10a4dbde6b29dc356897e0ddb1f836
manasik <manasi.kher@leftrightmind.com>
2018-02-23 14:28:54 +0530

    Minor fixes done inside the code

--- app/src/main/java/dayfive/com/lrm/kotlinday5/map_details/MapsActivity.kt
+++ app/src/main/java/dayfive/com/lrm/kotlinday5/map_details/MapsActivity.kt
@@ -2,6 +2,7 @@ package dayfive.com.lrm.kotlinday5.map_details
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
 import android.content.pm.PackageManager
@@ -23,6 +24,9 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import dayfive.com.lrm.kotlinday5.R
 import dayfive.com.lrm.kotlinday5.gps_location_tracker.GpsTracker
+import android.content.Intent
+import android.widget.Toast
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -38,6 +42,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationManager: LocationManager
     lateinit var handler: Handler;
 
+    val ACCESS_FINE_LOCATION = 1
+    val ACCESS_COARSE_LOCATION= 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -52,7 +59,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-    @SuppressLint("MissingPermission")
+
     fun loadMapData()
     {
         mapFragment = supportFragmentManager
@@ -60,8 +67,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         gpsTracker = GpsTracker(this)
-        locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
 
     }
 
@@ -74,47 +79,102 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
-
-
+    override fun onMapReady(googleMap1: GoogleMap) {
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 2)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
         } else {
 
-            if (gpsTracker.canGetLoaction()) {
+           gpsTracker(googleMap1)
+           //googleMap= googleMap1
+        }
 
-                gpsTracker.onLocationChanged(location)
-                latitude = gpsTracker.getLatitude()
-                longitude = gpsTracker.getLongitude()
+    }
 
-                mMap = googleMap
+    fun gpsTracker(googleMap1: GoogleMap)
+    {
+        if (gpsTracker.canGetLoaction()) {
 
-                // Add a marker in Sydney and move the camera
+            locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            gpsTracker.onLocationChanged(location)
+            latitude = gpsTracker.getLatitude()
+            longitude = gpsTracker.getLongitude()
 
-                Handler().postDelayed({
-                    val pune = LatLng(latitude, longitude)
-                    mMap.addMarker(MarkerOptions().position(pune).title("Marker in Pune"))
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(pune))
-                    mMap.animateCamera(CameraUpdateFactory.zoomIn());
-                    mMap.animateCamera(CameraUpdateFactory.zoomTo(8.0f));
-                    //mMap.setMaxZoomPreference(14.0f);
-                    mMap.maxZoomLevel
-                }, 1500)
+            mMap = googleMap1
+
+            // Add a marker in Sydney and move the camera
+
+            Handler().postDelayed({
+                val pune = LatLng(latitude, longitude)
+                mMap.addMarker(MarkerOptions().position(pune).title("Marker in Pune"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(pune))
+                mMap.animateCamera(CameraUpdateFactory.zoomIn());
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(8.0f));
+                //mMap.setMaxZoomPreference(14.0f);
+                mMap.maxZoomLevel
+            }, 1500)
 
 
-                val builder = AlertDialog.Builder(this)
-                builder.setCancelable(true)
-                builder.setTitle("Location")
-                builder.setMessage("This is your current location: Latitude: $latitude Longitude: $longitude")
-                builder.setPositiveButton("OK") { dialogInterface, i -> dialogInterface.dismiss() }
-                builder.show()
+            val builder = AlertDialog.Builder(this)
+            builder.setCancelable(true)
+            builder.setTitle("Location")
+            builder.setMessage("This is your current location: Latitude: $latitude Longitude: $longitude")
+            builder.setPositiveButton("OK") { dialogInterface, i -> dialogInterface.dismiss() }
+            builder.show()
 
-            } else {
-                gpsTracker.openSettings()//Open the settings alert to enable the GPS sevice
+        } else {
+            gpsTracker.openSettings()//Open the settings alert to enable the GPS sevice
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(permsRequestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+
+        when (permsRequestCode) {
+
+            0 -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), ACCESS_COARSE_LOCATION)        //gpsTracker()
+                }else if (grantResults[0] == PackageManager.PERMISSION_DENIED){
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this@MapsActivity, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        //Show an explanation to the user *asynchronously*
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)        //gpsTracker()
+                    }else{
+                        val builder = AlertDialog.Builder(this)
+                        builder.setCancelable(true)
+                        builder.setTitle("Alert")
+                        builder.setMessage("Location permission is required")
+                        builder.setPositiveButton("OK") { dialogInterface, i -> dialogInterface.dismiss() }
+                        builder.show()
+                    }
+                }
+
+
+            }
+
+            1 -> {
+
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                  loadMapData()
+                }else if (grantResults[0] == PackageManager.PERMISSION_DENIED){
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this@MapsActivity, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                        //Show an explanation to the user *asynchronously*
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), ACCESS_COARSE_LOCATION)        //gpsTracker()
+                    }else{
+                        val builder = AlertDialog.Builder(this)
+                        builder.setCancelable(true)
+                        builder.setTitle("Alert")
+                        builder.setMessage("Location permission is required")
+                        builder.setPositiveButton("OK") { dialogInterface, i -> dialogInterface.dismiss() }
+                        builder.show()
+                    }
+                }
             }
         }
 
     }
+
+
+
 }
